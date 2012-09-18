21110825c6701a2613da8eedca63eeaef637ac88
Josh Merchant <josh@lymbix.com>
2012-09-18 12:50:43 -0400

    Updating version

--- lib/linked_in/version.rb
+++ lib/linked_in/version.rb
@@ -3,7 +3,7 @@ module LinkedIn
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 3
-    PATCH = 8
+    PATCH = 9
     PRE   = nil
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
   end
